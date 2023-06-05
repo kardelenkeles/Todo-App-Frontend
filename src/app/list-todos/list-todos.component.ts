43039808c78f019298a1b30a24bf9314fb1461fc
kardelenkeles <kar_di44@hotmail.com>
2023-06-05 15:43:24 +0300

    update routing

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TodoDataService} from "../service/data/todo-data.service";
+import {Router} from "@angular/router";
 
 export class Todo {
   constructor(
@@ -17,21 +18,23 @@ export class Todo {
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
- // @ts-ignore
+  // @ts-ignore
   todos: Todo[]
 
   // @ts-ignore
-  message:string
+  message: string
 
   constructor(
     private todoService: TodoDataService,
+    private router: Router
   ) {
   }
 
   ngOnInit() {
     this.refreshTodos();
   }
-  refreshTodos(){
+
+  refreshTodos() {
     this.todoService.retrieveAllTodos('keles').subscribe(
       response => {
         console.log(response);
@@ -40,14 +43,18 @@ export class ListTodosComponent implements OnInit {
     )
   }
 
-  deleteTodo(id:number){
-    this.todoService.deleteTodo('keles',id).subscribe(
+  deleteTodo(id: number) {
+    this.todoService.deleteTodo('keles', id).subscribe(
       response => {
         console.log(response);
         this.message = `Delete of Todo ${id} Successful!`
         this.refreshTodos();
       }
     )
+  }
+
+  updateTodo(id: number) {
+    this.router.navigate(['todos',id])
   }
 
 }
