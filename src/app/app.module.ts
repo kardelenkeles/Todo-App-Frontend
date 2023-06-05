5d68d19363915d1976be528ce41b022e20526fd9
kardelenkeles <kar_di44@hotmail.com>
2023-06-05 16:16:38 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {FooterComponent} from "./footer/footer.component";
 import {LogoutComponent} from "./logout/logout.component";
 import {RouteGuardService} from "./service/route-guard.service";
 import {HttpClientModule} from "@angular/common/http";
+import {TodoComponent} from "./todo/todo.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {HttpClientModule} from "@angular/common/http";
     ListTodosComponent,
     MenuComponent,
     FooterComponent,
-    LogoutComponent
+    LogoutComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TodoDataService} from "../service/data/todo-data.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
 
+  id: number
+  todo: Object
+
+  constructor(
+    private todoService: TodoDataService,
+    private route :ActivatedRoute
+  ) {
+  }
+
+  ngOnInit() {
+    this.id=this.route.snapshot.params['id']
+    this.todoService.retrieveTodo('keles', this.id)
+      .subscribe(
+        data => this.todo = data
+      )
+  }
 }
